52c390d3427f20e39b935491d215d2e9d1c560d7
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-03-27 19:54:26 -0400

    imported the main logo in React

--- nautilus/src/App.js
+++ nautilus/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import './App.css';
-
+import mainLogo from'./assets/global/nautilus-logo-sprite@2x.png';
 function App() {
   return (
     <div id="container">
       <nav>
-        
+      <img src={mainLogo} id="logo" />
       </nav>
     </div>
   );
