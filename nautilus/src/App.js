c1124fabab98a08e002801212f7c810f00b2f5d1
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-03-27 20:05:42 -0400

    added the second headline

--- nautilus/src/App.js
+++ nautilus/src/App.js
@@ -20,6 +20,7 @@ function App() {
       </nav>
       <div id="headline">
         <h4>THE NAUTILUS SPOTLIGHT</h4>
+        <h1>WHY THE LAWS OF PHYSICS ARE INVEVITABLE</h1>
       </div>
     </div>
   );
