98c0c7fbde33aa307f509c8ad1b865a30928112e
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-03-27 20:09:48 -0400

    created the sidebar

--- nautilus/src/App.js
+++ nautilus/src/App.js
@@ -24,7 +24,35 @@ function App() {
         <button>Read Now</button>
       </div>
       <div id="main">
-        
+        <div id="leftBar">
+                <div id="starterPost">
+                    <p><b>Issue 079</b></p>
+                    <h2>Catalyst</h2>
+                    <p id="quote">"What Generates a new theory in science?...."</p>
+                </div>
+                <ul id="post">
+                    <li>
+                        <p>Chapter 1</p>
+                        <p>Art</p>
+                        <p class="date"></p>
+                    </li>
+                    <li>
+                        <p>Chapter 2</p>
+                        <p>Mind</p>
+                        <p class="date">Coming December 12</p>
+                    </li>
+                    <li>
+                        <p>Chapter 3</p>
+                        <p>Energy</p>
+                        <p class="date">Coming December 19</p>
+                    </li>
+                    <li>
+                        <p>Chapter 4</p>
+                        <p>Stars</p>
+                        <p  class="date">Coming December 26</p>
+                    </li>
+                </ul>
+            </div>
       </div>
     </div>
   );
